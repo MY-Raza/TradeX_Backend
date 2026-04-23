1d245a772b34e8fab19d6f22f0da6d141b14d4ff
MY-Raza <yasirrazaattari2002@gmail.com>
2026-04-23 12:38:41 +0500

    CORSMiddleware Configuration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-from app.api.routes import strategies, models,data,backtest,sentiment  # only import what exists
+from app.api.routes import strategies, models,data,backtest,sentiment 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="TradeX API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(strategies.router)
 app.include_router(models.router)
